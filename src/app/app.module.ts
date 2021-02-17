e7ac7254bda78e8397d400806bdf42165044a6a6
nchaftari <58465694+nchaftari@users.noreply.github.com>
2021-02-18 00:31:47 +0200

    fixed footer and product ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
